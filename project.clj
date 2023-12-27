8654cfe887dc53b0a5f2e4d75aac688d8d5bebeb
Michell Campos <michell.campos@tokstok.com.br>
2023-12-27 10:25:02 -0300

    Added project dependencies

--- project.clj
+++ project.clj
@@ -2,6 +2,17 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]]
+            :url  "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [compojure "1.6.3"]
+                 [ring/ring-core "1.9.6"]
+                 [ring/ring-json "0.5.1"]
+                 [korma "0.3.0-RC5"]
+                 [mysql/mysql-connector-java "5.1.6"]
+                 [ring/ring-defaults "0.2.1"]]
+  :plugins [[lein-ring "0.9.7"]]
+  :ring {:handler todo.handler/app}
+  :profiles
+  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                        [ring/ring-mock "0.3.0"]]}}
   :repl-options {:init-ns todo.core})
