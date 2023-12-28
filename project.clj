851ec2bc853de3788d9d43b8475fab02e3ab7fef
Michell Campos <michell.campos@tokstok.com.br>
2023-12-28 19:12:28 -0300

    Added and removed some dependencies

--- project.clj
+++ project.clj
@@ -1,18 +1,16 @@
 (defproject todo "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url  "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]
-                 [compojure "1.6.3"]
+  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [compojure "1.6.1"]
+                 [ring/ring-defaults "0.3.2"]
+                 [ring/ring-json "0.4.0"]
                  [ring/ring-core "1.9.6"]
-                 [ring/ring-json "0.5.1"]
-                 [korma "0.3.0-RC5"]
-                 [mysql/mysql-connector-java "5.1.6"]
-                 [ring/ring-defaults "0.2.1"]]
-  :plugins [[lein-ring "0.9.7"]]
+                 [korma "0.4.3"]
+                 [org.xerial/sqlite-jdbc "3.32.3"]]
+  :plugins [[lein-ring "0.12.5"]]
   :ring {:handler todo.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring/ring-mock "0.3.0"]]}}
-  :repl-options {:init-ns todo.core})
+                        [ring/ring-mock "0.3.2"]]}})
