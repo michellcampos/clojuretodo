dd5fd30bc3524e0af7e8d165964420a6d3f548be
Michell Campos <michell.campos@tokstok.com.br>
2023-12-29 08:10:28 -0300

    Added routes for GET by ID, PUT to update a record by ID and DELETE to delete a record by ID

--- src/todo/handler.clj
+++ src/todo/handler.clj
@@ -12,11 +12,19 @@
 (defroutes app-routes
   (GET "/" [] "Hello World")
   (GET "/api/todos" [] (response (get-todos)))
+  (GET "/api/todos/:id" [id]
+    (response (get-todo (Integer/parseInt id))))
   (POST "/api/todos" {params :params}
     (let [title (:title params)
           description (:description params)]
       (add-todo title description)
       (response {:status "success"})))
+  (PUT "/api/todos/:id" [id title description]
+      (update-todo (Integer/parseInt id) title description)
+      (response {:status "success"}))
+  (DELETE "/api/todos/:id" [id]
+    (delete-todo (Integer/parseInt id))
+    (response {:status "success"}))
   (route/not-found "Not Found"))
 
 (def app
